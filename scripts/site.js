3b3e509391f6f76b65f4b2df80f7701a479ffaef
Bolling <bolling@Bollings-MacBook-Pro-2.local>
2017-01-06 16:30:48 -0500

    Toggle faq on and off

--- scripts/site.js
+++ scripts/site.js
@@ -1,3 +1,18 @@
 // Main JS should go here!
 // Include scripts using Browserify by doing:
-// var $ = require("jquery");
+var $ = require("jquery");
+
+
+    $(document).on('click','.faqs__faq--closed', function(e) {
+        $(this).removeClass("faqs__faq--closed");
+        $(this).addClass("faqs__faq");
+    });
+    
+    $(document).on('click', '.faqs__faq', function() {
+        $(this).removeClass("faqs__faq");
+        $(this).addClass("faqs__faq--closed");
+    });
+
+
+
+
