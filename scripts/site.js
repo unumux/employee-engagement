a726f3bf0913cf8ea7ecd9bd7f2ccd09565e585e
Bolling <bolling@macbookpro-456a.up.corp.upc>
2017-01-10 15:59:16 -0500

    Work on carousel

--- scripts/site.js
+++ scripts/site.js
@@ -14,5 +14,41 @@ var $ = require("jquery");
     });
 
 
+var currentIndex = 0,
+  items = $('.page-header__container div'),
+  itemAmt = items.length;
+
+function cycleItems() {
+  var item = $('.page-header__container div').eq(currentIndex);
+  items.hide();
+  item.css('display','inline-block');
+}
+
+var autoSlide = setInterval(function() {
+  currentIndex += 1;
+  if (currentIndex > itemAmt - 1) {
+    currentIndex = 0;
+  }
+  cycleItems();
+}, 10000);
+
+// $('.next').click(function() {
+//   clearInterval(autoSlide);
+//   currentIndex += 1;
+//   if (currentIndex > itemAmt - 1) {
+//     currentIndex = 0;
+//   }
+//   cycleItems();
+// });
+
+// $('.prev').click(function() {
+//   clearInterval(autoSlide);
+//   currentIndex -= 1;
+//   if (currentIndex < 0) {
+//     currentIndex = itemAmt - 1;
+//   }
+//   cycleItems();
+// });
+
 
 
