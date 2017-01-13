555de7ba55b7d50f3695e29df0184e9b6b7db850
Bolling <bolling@Bollings-MacBook-Pro-2.local>
2017-01-13 09:48:51 -0500

    Slider update and header

--- scripts/site.js
+++ scripts/site.js
@@ -12,38 +12,11 @@ $(".faqs").on("click", "[data-collapsed='false'] .faqs__question", function () {
     $(this).parent().attr("data-collapsed", "true").find(".faqs__answer").slideUp();
 });
 
+
+//Carousel 
 $('.carousel').slick({
-    dots: true
+    dots: true,
+    arrows: false,
+    autoplay: true,
+    autoplaySpeed: 4000
 });
-
-// Carousel
-// var currentIndex = 0,
-//   items = $('.page-header__container li'),
-//   itemAmt = items.length;
-
-// function cycleItems() {
-//   var item = $('.page-header__container li').eq(currentIndex);
-//   items.hide();
-//   item.css('display','inline-block');
-// }
-
-// var autoSlide = setInterval(function() {
-//   currentIndex += 1;
-//   if (currentIndex > itemAmt - 1) {
-//     currentIndex = 0;
-//   }
-//   cycleItems();
-// }, 3000);
-
-//My shot at trying to make the dots on the header clickable
-// $(document).ready(function () {
-//     $('ol li').bind('click', function(){
-//         var index = $(this).index() + 1;
-
-//         $(".active").fadeOut(300);
-
-//         $(".slider_" + index).fadeIn(300);        
-//         $("li .slide__image").removeClass("active");
-//         $(".slider_" + index).addClass("active");
-//     });
-// })
