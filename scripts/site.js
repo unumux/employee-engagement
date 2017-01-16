59c819889c4baf05a920019dfa026e8a457a1b2a
Bolling <bolling@macbookpro-456a.up.corp.upc>
2017-01-16 14:51:57 -0500

    JS fix

--- scripts/site.js
+++ scripts/site.js
@@ -1,23 +1,24 @@
 // Main JS should go here!
 
+$(document).ready(function() {
+    // FAQs
+    $(".faqs__faq").attr("data-collapsed", "true").find(".faqs__answer").hide();
 
-// FAQs
-$(".faqs__faq").attr("data-collapsed", "true").find(".faqs__answer").hide();
+    $(".faqs").on("click", "[data-collapsed='true'] .faqs__question", function () {
+        $(this).parent().attr("data-collapsed", "false").find(".faqs__answer").slideDown();
+    });
 
-$(".faqs").on("click", "[data-collapsed='true'] .faqs__question", function () {
-    $(this).parent().attr("data-collapsed", "false").find(".faqs__answer").slideDown();
-});
-
-$(".faqs").on("click", "[data-collapsed='false'] .faqs__question", function () {
-    $(this).parent().attr("data-collapsed", "true").find(".faqs__answer").slideUp();
-});
+    $(".faqs").on("click", "[data-collapsed='false'] .faqs__question", function () {
+        $(this).parent().attr("data-collapsed", "true").find(".faqs__answer").slideUp();
+    });
 
 
-//Carousel 
-$('.carousel').slick({
-    dots: true,
-    arrows: false,
-    autoplay: true,
-    autoplaySpeed: 4000,
-    swipeToSlide: true
+    //Carousel 
+    $('.carousel').slick({
+        dots: true,
+        arrows: false,
+        autoplay: true,
+        autoplaySpeed: 4000,
+        swipeToSlide: true
+    });
 });
